24f2976f3b98b8325caec7a1b7631bba439260b5
Lichkatanz <stu2113811027@uni-plovdiv.bg>
2024-12-18 11:41:01 +0200

    upp

---
+++ MovieStore/MovieStore.Models/DTO/Actor.cs
@@ -0,0 +1,10 @@
+namespace MovieStore.Models.DTO
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+            = string.Empty;
+    }
+}
