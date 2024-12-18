0d66f308c7b2ca1ccbfd832d4221c9f17f8fa9f6
Lichkatanz <stu2113811027@uni-plovdiv.bg>
2024-12-18 11:37:14 +0200

    upp

---
+++ MovieStore/MovieStore.DL/DependencyInjection.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using MovieStore.DL.Interfaces;
+using MovieStore.DL.Repositories;
+using MovieStore.DL.Repositories.MongoRepositories;
+
+namespace MovieStore.DL
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IMovieRepository, MovieRepository>()
+                .AddSingleton<IActorRepository, ActorStaticRepository>();
+        }
+    }
+}
